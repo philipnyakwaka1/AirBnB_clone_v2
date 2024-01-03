c4abbdebeb8c752d42d514aa495bb6a628b4f46a
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-03 12:46:06 +0300

    commited

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,7 @@ sudo touch /data/web_static/releases/test/index.html
 sudo echo "I am learning Fabric and Deploying Static" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\talias data/web_static/current/\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo service nginx restart
