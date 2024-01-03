46010400f6f7ecfe672934ff8a8ae932e47d4ae8
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-03 13:59:19 +0300

    up

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the
+contents of the web_static folder of your AirBnB Clone repo,
+using the function do_pack
+"""
+import os
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    generates a .tgz archive from the contents of
+    the web_static folder of your AirBnB Clone repo
+    """
+    try:
+        local("mkdir -p versions")
+        t = datetime.now()
+        archive_name = 'web_static_{}{}{}{}{}{}.tgz'.format(
+            t.year, t.month, t.day, t.hour, t.minute, t.second)
+        local("tar -cvzf versions/{}.tgz web_static".format(archive_name))
+        return f'versions/{archive_name}'
+    except Exception as e:
+        print(str(e))
--- test.py
+++
@@ -1,3 +0,0 @@
-my_dict = {'name': 'Philip', 'age': 25}
-if 'name' in my_dict and 'age' in my_dict:
-    print('Both Present')
